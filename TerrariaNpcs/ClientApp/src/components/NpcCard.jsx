d02ef2be88cb06f40253f978a4936fb518483ec9
yojay esteban <yojayhortaeb@unimagdalena.edu.co>
2023-10-31 16:41:11 -0500

    last changes - corrected mobile view

--- TerrariaNpcs/ClientApp/src/components/NpcCard.jsx
+++ TerrariaNpcs/ClientApp/src/components/NpcCard.jsx
@@ -14,7 +14,6 @@ const NpcCard = ({ npc, deleteModal, getNpcs, page }) => {
         navigate(path);
     }
     const [loading, setLoading] = useState(false);
-
     const deleteNpc = async() => {
         
         try {
@@ -38,7 +37,7 @@ const NpcCard = ({ npc, deleteModal, getNpcs, page }) => {
             <img src="./pasto.png" style={{ width: "180px" }} alt="grass terraria"></img>
             <div className="card rounded-0 cardInt pt-1">
                 <div className="d-flex justify-content-around w-100 text-center">
-                    <h4 className="card-title w-75">{npc.name}</h4>
+                    <h4 className={` card-title w-75 ${npc.name.length < 13 ? '' : npc.name.length < 16 ? 'fs-5 m-2' : 'fs-6 m-2'}`}>{npc.name}</h4>
                     {deleteModal && !loading ? <button type="button" disabled={loading} className="btn p-0 w-20"
                         onClick={deleteNpc} >
                         <PiTrashBold size={25} className="iconDelete" />
@@ -66,7 +65,7 @@ const NpcCard = ({ npc, deleteModal, getNpcs, page }) => {
                             {npc.biome}
                         </div>
                     </div>
-                    Job:<p> {npc.speciality}</p>
+                    Job:<p > {npc.speciality}</p>
                     
                 </div>
             </div>
